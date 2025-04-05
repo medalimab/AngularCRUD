84282e8689c23a049a14fa4e26804e65b05e9cde
dali <mabroukdali1925@gmail.com>
2025-04-06 00:58:36 +0200

    update

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,11 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
 
 @Component({
   selector: 'app-employee-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,HttpClientModule],
   templateUrl: './employee-dashboard.component.html',
-  styleUrl: './employee-dashboard.component.css'
+  styleUrl: './employee-dashboard.component.css',
+  providers: [ ApiService],
 })
 export class EmployeeDashboardComponent {
 
