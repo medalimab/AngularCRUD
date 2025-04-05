f241f6f4cf79964dedee9789334fd10a58ba4aaf
dali <mabroukdali1925@gmail.com>
2025-04-05 23:21:29 +0200

    empty project

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import e from 'express';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,EmployeeDashboardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'AngularCRUD';
+}
