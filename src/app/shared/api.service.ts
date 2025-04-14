9e8442c8ce8738498eb7b81492e18ad26dbd2ce7
dali <mabroukdali1925@gmail.com>
2025-04-14 15:19:52 +0200

    update service.ts

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -12,9 +12,21 @@ export class ApiService {
   .pipe(map((res:any)=>{ return res;
   }))
   }
-  getEmployee(data :any){
+  getAllEmployee(){
   return this.http.get<any>("http://localhost:3000/posts")
   .pipe(map((res:any)=>{ return res;
   }))
   }
+  deleteEmployee(id: any){
+  return this.http.delete<any>("http://localhost:3000/posts/"+id)
+  .pipe(map((res:any)=>{ return res;
+  }))
+
   }
+
+
+  updadteEmployee(data : any, id: number){
+  return this.http.put<any>("http://localhost:3000/posts/"+id, data)
+  .pipe(map((res:any)=>{ return res;}))}
+
+}
